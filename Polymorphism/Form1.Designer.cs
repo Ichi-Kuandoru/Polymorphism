f3ce4e29ffa18188cf71e72773f2279bae8e5f63
Ichi Kuandoru <mister.nik2002@yandex.ru>
2024-06-25 19:12:57 +0700

    Добавьте файлы проекта.

---
+++ Polymorphism/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Polymorphism
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(402, 256);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 275);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel2);
+            this.groupBox2.Location = new System.Drawing.Point(426, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(415, 275);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 16);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(409, 256);
+            this.panel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 325);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Polymorphism";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Polymorphism/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Polymorphism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Создаем тестовые уравнения
+            Equation sineEquation = new SineEquation(2.0);
+            Equation absoluteSineEquation = new AbsoluteSineEquation(3.0);
+
+            // Рисуем графики
+            DrawFunction(-10.0, 10.0, sineEquation, panel1);
+            DrawFunction(-10.0, 10.0, absoluteSineEquation, panel2);
+        }
+
+        private void DrawFunction(double x1, double x2, Equation equation, Panel panel)
+        {
+            // Создаем bitmap для рисования
+            Bitmap bitmap = new Bitmap(panel.Width, panel.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            // Настраиваем систему координат
+            g.TranslateTransform(panel.Width / 2, panel.Height / 2);
+            g.ScaleTransform(panel.Width / 20.0f, -panel.Height / 20.0f);
+
+            // Рисуем график
+            g.Clear(Color.White);
+            g.DrawLine(Pens.Black, (float)x1, 0, (float)x2, 0);
+            g.DrawLine(Pens.Black, 0, (float)x1, 0, (float)x2);
+
+            for (double x = x1; x <= x2; x += 0.1)
+            {
+                float y = (float)equation.Evaluate(x);
+                g.DrawLine(Pens.Black, (float)x, 0, (float)x, y);
+            }
+
+            // Отображаем bitmap на панели
+            panel.CreateGraphics().DrawImage(bitmap, 0, 0);
+        }
+    }
+
+    // Абстрактный класс Equation
+    public abstract class Equation
+    {
+        public abstract double Evaluate(double x);
+    }
+
+    // Производный класс для уравнения sin(a*x)/x
+    public class SineEquation : Equation
+    {
+        private readonly double a;
+        public SineEquation(double a)
+        {
+            this.a = a;
+        }
+        public override double Evaluate(double x)
+        {
+            if (x == 0)
+                return 0;
+            else
+                return Math.Sin(a * x) / x;
+        }
+    }
+
+    // Производный класс для уравнения a*x*|sin(x)|
+    public class AbsoluteSineEquation : Equation
+    {
+        private readonly double a;
+        public AbsoluteSineEquation(double a)
+        {
+            this.a = a;
+        }
+        public override double Evaluate(double x)
+        {
+            return a * x * Math.Abs(Math.Sin(x));
+        }
+    }
+}
